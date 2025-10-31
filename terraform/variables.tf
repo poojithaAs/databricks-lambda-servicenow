9ebed6789df4841994ecf42d20c32065915b1d7b
poojithaAs <49402197+poojithaAs@users.noreply.github.com>
2025-10-31 05:35:16 -0500

    Add variables for AWS and Databricks configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,5 @@
-
+variable "aws_region"        { default = "us-east-1" }
+variable "databricks_host"   {}
+variable "databricks_http_path" {}
+variable "databricks_token"  {}
+variable "env"               { default = "dev" }
