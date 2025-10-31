4f10a46c6c8d55a849a3cdae72fe6177673b1caa
poojithaAs <49402197+poojithaAs@users.noreply.github.com>
2025-10-31 11:56:07 -0500

    Refactor IAM role and remove unused resources
    Refactor IAM role creation and logging policy attachment. Remove unused API Gateway resources and Lambda function configuration.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,70 +1,54 @@
-provider "aws" { region = var.aws_region }
+# Try to find existing role first
+data "aws_iam_role" "existing_lambda_role" {
+  name = "databricks_lambda_role"
+}
 
+# Create the role only if it doesn't exist
 resource "aws_iam_role" "lambda_role" {
+  count = length(data.aws_iam_role.existing_lambda_role.*.id) == 0 ? 1 : 0
+
   name = "databricks_lambda_role"
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Action = "sts:AssumeRole",
-      Principal = { Service = "lambda.amazonaws.com" }
-    }]
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
   })
 }
 
+# Unified reference for later use
+locals {
+  lambda_role_arn = length(data.aws_iam_role.existing_lambda_role.*.arn) > 0 ?
+                    data.aws_iam_role.existing_lambda_role.arn :
+                    aws_iam_role.lambda_role[0].arn
+  lambda_role_id = length(data.aws_iam_role.existing_lambda_role.*.id) > 0 ?
+                    data.aws_iam_role.existing_lambda_role.id :
+                    aws_iam_role.lambda_role[0].id
+}
+
+# Attach CloudWatch logging policy
 resource "aws_iam_role_policy" "lambda_logs" {
-  name   = "lambda_logging"
-  role   = aws_iam_role.lambda_role.id
+  name = "lambda_logging"
+  role = local.lambda_role_id
+
   policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect   = "Allow",
-      Action   = ["logs:CreateLogGroup","logs:CreateLogStream","logs:PutLogEvents"],
-      Resource = "arn:aws:logs:*:*:*"
-    }]
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
   })
 }
-
-resource "aws_lambda_function" "databricks_lambda" {
-  function_name = "databricks_lambda_function"
-  handler       = "main.lambda_handler"
-  runtime       = "python3.12"
-  role          = aws_iam_role.lambda_role.arn
-  filename      = "../lambda.zip"
-  timeout       = 15
-  environment {
-    variables = {
-      DATABRICKS_HOST      = var.databricks_host
-      DATABRICKS_HTTP_PATH = var.databricks_http_path
-      DATABRICKS_TOKEN     = var.databricks_token
-    }
-  }
-}
-
-resource "aws_apigatewayv2_api" "lambda_api" {
-  name          = "databricks_lambda_api"
-  protocol_type = "HTTP"
-}
-
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id           = aws_apigatewayv2_api.lambda_api.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.databricks_lambda.invoke_arn
-}
-
-resource "aws_apigatewayv2_route" "lambda_route" {
-  api_id    = aws_apigatewayv2_api.lambda_api.id
-  route_key = "POST /query"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
-
-resource "aws_apigatewayv2_stage" "lambda_stage" {
-  api_id      = aws_apigatewayv2_api.lambda_api.id
-  name        = var.env
-  auto_deploy = true
-}
-
-output "invoke_url" {
-  value = "${aws_apigatewayv2_stage.lambda_stage.invoke_url}/query"
-}
-
