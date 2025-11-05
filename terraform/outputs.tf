ec1f5ed79b92454948615c06639b761ec388ea33
poojithaAs <49402197+poojithaAs@users.noreply.github.com>
2025-11-04 22:12:03 -0600

    Update outputs for Lambda and API Gateway

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,9 @@
 output "lambda_name" {
-  value = aws_lambda_function.databricks_trigger.function_name
+  description = "Lambda function name"
+  value       = aws_lambda_function.databricks_lambda.function_name
 }
 
-output "api_url" {
-  value = aws_apigatewayv2_api.databricks_api.api_endpoint
+output "api_gateway_url" {
+  description = "Invoke URL for ServiceNow"
+  value       = aws_apigatewayv2_api.lambda_api.api_endpoint
 }
