a7b7c31dc2ef1f9e32d61c6dcec063e210c7cd47
poojithaAs <49402197+poojithaAs@users.noreply.github.com>
2025-11-05 05:53:11 -0600

    Define default values and descriptions for variables
    Added default values and descriptions for several variables in the Terraform configuration.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,23 @@
-variable "aws_region" {}
-variable "project_name" {}
-variable "subnet_ids" {
-  type = list(string)
-}
-variable "security_group_ids" {
-  type = list(string)
-}
+variable "aws_region" { default = "us-east-1" }
+variable "project_name" { default = "servicenow-databricks" }
+
+# Databricks values (used to seed Secrets Manager only)
 variable "databricks_url" {}
-variable "databricks_token" {}
+variable "databricks_token" { sensitive = true }
 variable "databricks_job_id" {}
+
+# Existing VPC details
+variable "vpc_id" {
+  description = "Existing VPC ID"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of private subnet IDs"
+  type        = list(string)
+}
+
+variable "security_group_ids" {
+  description = "Security groups for Lambda"
+  type        = list(string)
+}
