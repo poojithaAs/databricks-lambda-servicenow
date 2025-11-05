7a8f29e6afd2574d4fcfee31eb3777a108a9d9c4
poojithaAs <49402197+poojithaAs@users.noreply.github.com>
2025-11-04 22:13:17 -0600

    Disable outputs in outputs.tf
    Comment out outputs for lambda_name and api_gateway_url.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,3 @@
-output "lambda_name" {
-  description = "Lambda function name"
-  value       = aws_lambda_function.databricks_lambda.function_name
-}
-
-output "api_gateway_url" {
-  description = "Invoke URL for ServiceNow"
-  value       = aws_apigatewayv2_api.lambda_api.api_endpoint
-}
+# output "api_gateway_url" {
+#   value = aws_apigatewayv2_api.lambda_api.api_endpoint
+# }
