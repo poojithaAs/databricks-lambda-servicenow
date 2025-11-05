95d030ceca0f36c5d62189d34f60b66c50728896
poojithaAs <49402197+poojithaAs@users.noreply.github.com>
2025-11-04 21:24:49 -0600

    Enhance variable definitions in variables.tf
    Added descriptions for existing variables and new variables for Databricks job ID and token ARN.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,16 @@
-variable "aws_region"        { default = "us-east-1" }
-variable "databricks_host"   {}
-variable "databricks_http_path" {}
-variable "databricks_token"  {}
-variable "env"               { default = "dev" }
+variable "region" {
+  description = "AWS Region"
+  default     = "us-east-1"
+}
+
+variable "databricks_host" {
+  description = "Databricks workspace base URL"
+}
+
+variable "job_id" {
+  description = "Databricks job ID"
+}
+
+variable "databricks_token_secret_arn" {
+  description = "ARN of the secret in AWS Secrets Manager containing Databricks token"
+}
